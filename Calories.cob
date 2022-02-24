41e21bbc38bec7f9e0417bd527f1b7c9f685fa42
Kai Lyons <kai@potabi.com>
2022-02-24 00:20:00 -0700

    Finish up with report maker

--- Calories.cob
+++ Calories.cob
@@ -14,17 +14,27 @@
        DATA DIVISION.
        FILE SECTION. 
        FD CaloriesLogfile.
-       01 LOG-CalorieEntry.
-           02 LOG-EatenAtTime.
-              03 LOG-Hour          PIC 99.
-              03 LOG-Minute        PIC 99.
-           02 LOG-CaloricCount     PIC 9(3).
-           02 LOG-EntryDescription PIC X(50).
+       01 LOG-PrintLine            PIC X(75).
+       88 LOG-EndOfFile            VALUE 'Y' FALSE 'N'.
+       01 FD-CalorieEntry.
+           02 FD-EatenAtTime.
+              03 FD-Hour          PIC 99.
+              03 FD-Minute        PIC 99.
+           02 FD-CaloricCount     PIC 9(3).
+           02 FD-EntryDescription PIC X(50).
        FD CaloricReport.
-       01 RPT-Header.
-           02 FILLER               PIC X(5)  VALUE "TIME ".
-           02 FILLER               PIC X(9)  VALUE "CALORIES ".
-           02 FILLER               PIC X(12) VALUE "DESCRIPTION ".
+       01 RPT-PrintLine            PIC X(75).
+       WORKING-STORAGE SECTION.
+       01 WS-UserAction            PIC 9.
+       01 WS-LineCount             PIC 99 VALUE ZERO.
+       01 RPT-Heading.
+           02 FILLER               PIC X(20) 
+              VALUE "Caloric Report For: ".
+           02 RPT-NAME             PIC X(25).
+       01 RPT-ValueHeader.
+           02 FILLER               PIC X(7)  VALUE "TIME  |".
+           02 FILLER               PIC X(12) VALUE " CALORIES  |".
+           02 FILLER               PIC X(12) VALUE " DESCRIPTION".
        01 RPT-LINE.
            02 FILLER               PIC X(5)  VALUE "-----".
            02 FILLER               PIC X(3)  VALUE "-+-".
@@ -38,11 +48,15 @@
               03 FILLER            PIC X     VALUE ":".
               03 RPT-Minute        PIC 99.
            02 FILLER               PIC X(3)  VALUE " | ".
-           02 RPT-CaloricCount     PIC 9(3).
+           02 RPT-CaloricCount     PIC Z(9).
            02 FILLER               PIC X(3)  VALUE " | ".
            02 RPT-EntryDescription PIC X(50).
-       WORKING-STORAGE SECTION.   
-       01 WS-UserAction            PIC 9.
+       01 LOG-CalorieEntry.
+           02 LOG-EatenAtTime.
+              03 LOG-Hour          PIC 99.
+              03 LOG-Minute        PIC 99.
+           02 LOG-CaloricCount     PIC 9(3).
+           02 LOG-EntryDescription PIC X(50).
        PROCEDURE DIVISION.
            DISPLAY "[1] Create Entry".
            DISPLAY "[2] Create Report".
@@ -66,8 +80,39 @@
               ACCEPT LOG-CaloricCount.
               DISPLAY "Enter description (Up to 50 characters long)".
               ACCEPT LOG-EntryDescription.
-              WRITE LOG-CalorieEntry.
+              WRITE LOG-PrintLine FROM LOG-CalorieEntry.
            CLOSE CaloriesLogfile.
 
        0200-CreateReport.
+           DISPLAY "Enter name: " WITH NO ADVANCING. 
+           ACCEPT RPT-NAME.
            DISPLAY "Creating Report".
+           OPEN INPUT CaloriesLogfile.
+              OPEN OUTPUT CaloricReport.
+                 PERFORM 0210-PrintHeading.
+                 PERFORM 0220-PrintValueHeader.
+                 READ CaloriesLogfile
+                    AT END SET LOG-EndOfFile TO TRUE
+                 END-READ.
+                 PERFORM 0230-PrintCalorieEntry UNTIL LOG-EndOfFile.
+           CLOSE CaloriesLogfile,CaloricReport.
+
+       0210-PrintHeading.
+           WRITE RPT-PrintLine FROM RPT-Heading.
+       
+       0220-PrintValueHeader.
+           WRITE RPT-PrintLine FROM RPT-ValueHeader AFTER 3 LINES.
+           WRITE RPT-PrintLine FROM RPT-LINE AFTER 1 LINE.
+
+       0230-PrintCalorieEntry.
+           MOVE FD-Hour TO RPT-Hour.
+           MOVE FD-Minute TO RPT-Minute.
+           MOVE FD-CaloricCount TO RPT-CaloricCount.
+           MOVE FD-EntryDescription TO RPT-EntryDescription.
+           WRITE RPT-PrintLine FROM RPT-CalorieEntry AFTER 
+           ADVANCING 1 LINE.
+           ADD 1 TO WS-LineCount.
+           READ CaloriesLogfile
+              AT END SET LOG-EndOfFile TO TRUE
+           END-READ.
+           
