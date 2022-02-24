b3df08630a470d7528b49705aad36731ca70e97d
Kai Lyons <kai@potabi.com>
2022-02-23 22:54:10 -0700

    Add report file description

--- Calories.cob
+++ Calories.cob
@@ -9,15 +9,24 @@
            SELECT CaloriesLogfile ASSIGN TO "calories.log"
            ORGANIZATION IS LINE SEQUENTIAL
            ACCESS IS SEQUENTIAL.
+           SELECT CaloricReport ASSIGN TO "caloric-report.rpt"
+           ORGANIZAtION IS LINE SEQUENTIAL.
        DATA DIVISION.
        FILE SECTION. 
        FD CaloriesLogfile.
-       01 FD-CalorieEntry.
-           02 FD-EatenAtTime.
-              03 FD-Hour          PIC 99.
-              03 FD-Minute        PIC 99.
-           02 FD-CaloricCount     PIC 9(3).
-           02 FD-EntryDescription PIC X(50).
+       01 LOG-CalorieEntry.
+           02 LOG-EatenAtTime.
+              03 LOG-Hour          PIC 99.
+              03 LOG-Minute        PIC 99.
+           02 LOG-CaloricCount     PIC 9(3).
+           02 LOG-EntryDescription PIC X(50).
+       FD CaloricReport.
+       01 RPT-CalorieEntry.
+           02 RPT-EatenAtTime.
+              03 RPT-Hour          PIC 99.
+              03 RPT-Minute        PIC 99.
+           02 RPT-CaloricCount     PIC 9(3).
+           02 RPT-EntryDescription PIC X(50).
        WORKING-STORAGE SECTION.   
        01 WS-UserAction.
        PROCEDURE DIVISION.
