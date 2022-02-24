cbfb53388170b78baf31e8c32fe6dc0e926cfd79
Kai Lyons <kai@potabi.com>
2022-02-23 22:41:34 -0700

    Create variables

--- Calories.cob
+++ Calories.cob
@@ -6,6 +6,10 @@
        ENVIRONMENT DIVISION. 
        DATA DIVISION.
        WORKING-STORAGE SECTION. 
-
+       01 WS-CalorieEntry.
+           02 WS-EatenAtTime.
+              03 WS-Hour          PIC 99.
+              03 WS-Minute        PIC 99.
+           02 WS-CaloricCount     PIC 9(3).
+           02 WS-EntryDescription PIC X(50).
        PROCEDURE DIVISION.
-       
