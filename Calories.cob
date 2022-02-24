0d267745650230dcea9b2360adcf3d3b3c9d33b4
Kai Lyons <kai@potabi.com>
2022-02-23 22:30:54 -0700

    Create Calories.cob

---
+++ Calories.cob
@@ -0,0 +1,11 @@
+      * ----------------------------------------------------------------
+       IDENTIFICATION DIVISION. 
+       PROGRAM-ID. Caloric-Calculator.
+       AUTHOR. Kai Lyons.
+      * ----------------------------------------------------------------
+       ENVIRONMENT DIVISION. 
+       DATA DIVISION.
+       WORKING-STORAGE SECTION. 
+
+       PROCEDURE DIVISION.
+       
